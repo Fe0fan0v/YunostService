a20124c5d49846bd9c1c619ee3338f73c3d69e29
Vadim <Oxan4ikDanArt>
2022-05-04 22:44:42 +0500

    Enrolling page

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, session
+from flask_login import current_user
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main_page():
     return render_template('index.html')
+
+
+@app.route('/enroll')   # страница записи ребёнка
+def enroll():
+    return render_template('enroll.html')
 
 
 if __name__ == '__main__':
