8bf3e46b024c8736ec400360a63eccb1efd3ffef
Vadim <Oxan4ikDanArt>
2022-05-14 09:36:49 +0500

    Add new course

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from db import db_session
 from db.models import Course, User
+from forms import NewCourse, RegisterForm
+
 
 app = Flask(__name__)
 db_session.global_init()
+app.config['SECRET_KEY'] = 'super_secret_key'
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,11 +19,28 @@ def enroll():
     return render_template('enroll.html', title='Запись')
 
 
-@app.route('/admin')
+@app.route('/admin')    # панель администратора
 def admin_panel():
     db_sess = db_session.create_session()
     courses = db_sess.query(Course).all()
     return render_template('admin.html', title='Панель администратора', courses=courses)
+
+
+@app.route('/add_course', methods=['GET', 'POST'])   # добавление нового курса
+def add_course():
+    form = NewCourse()
+    if request.method == 'POST':
+        db_sess = db_session.create_session()
+        data = request.form
+        info = data.to_dict()
+        area = info['area']
+        direction = info['direction']
+        description = info['description']
+        groups = data.getlist('group[]')
+        weekdays = data.getlist('weekday[]')
+        times = data.getlist('time[]')
+        print(info)
+    return render_template('add_course.html', title='Добавить новое объединение', form=form)
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField, StringField, SubmitField, PasswordField, DateField, TelField, SelectField, IntegerField
+from wtforms import EmailField, StringField, SubmitField, PasswordField, DateField, TelField, SelectField, IntegerField, \
+    TextAreaField, TimeField
 from wtforms.validators import DataRequired
 
 
@@ -17,3 +18,26 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     submit = SubmitField('Зарегистрироваться')
+
+
+class NewCourse(FlaskForm):
+    name = StringField('Название курса', validators=[DataRequired()])
+    direction = SelectField('Направление курса', validators=[DataRequired()],
+                            choices=['Изобразительное искусство', 'Вокал', 'Хореография', 'Театр', 'Цирк',
+                                     'Развивающая акробатика', 'Журналистика', 'Психология',
+                                     'Декоративно-прикладное творчество (валяние из шерсти, бисероплетение, текстильная кукла и др.)',
+                                     'Конструирование, моделирование', 'Спорт', 'Шахматы', 'Английский язык', 'Подготовка к школе',
+                                     'Естественные науки (химия, биология, астрономия, геология)',
+                                     'IT (программирование, робототехника, технический английский и др.)',
+                                     'Фотография и видеография'])
+    area = SelectField('Площадка проведения курса', validators=[DataRequired()],
+                       choices=['Пр-т Макеева, 39', 'Пр-т Октября, 21', 'Ул. Ст. Разина, 4', 'Ул. 8-е марта, 147',
+                                'ул. Первомайская, 9'])
+    teacher = StringField('Фамилия Имя Отчество педагога', validators=[DataRequired()])
+    age_from = StringField('Начало диапазона возраста', validators=[DataRequired()])
+    age_to = StringField('Конец диапазона возраста', validators=[DataRequired()])
+    group = StringField('Номер группы', validators=[DataRequired()])
+    schedule_weekday = SelectField('День недели', validators=[DataRequired()])
+    schedule_time = TimeField('Время занятия', validators=[DataRequired()])
+    description = TextAreaField('Описание курса', validators=[DataRequired()])
+    submit = SubmitField('Сохранить', validators=[DataRequired()])
