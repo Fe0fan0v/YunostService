a6a82fd813654bf55a7d32e9553e633d22c0cf11
feofanov <feofanov@yandexlyceum.ru>
2022-05-17 23:34:33 +0500

    Add admin redacting page

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from db import db_session
 from db.models import Course
 from forms import NewCourse, RegisterForm, RegisterChild
 import urllib.parse
+from showing import show_courses
 from pprint import pprint
 
 app = Flask(__name__)
@@ -17,51 +18,21 @@ def main_page():
 
 @app.route('/enroll', methods=['GET', 'POST'])  # страница записи ребёнка
 def enroll():
-    db_sess = db_session.create_session()
-    sort_type = request.args.get('sort_type')
-    courses = db_sess.query(Course).all()
-    nav_areas = list(set([course.area for course in courses]))
-    if not sort_type:
-        pass
-    elif sort_type.split('_')[0] != 'age':
-        sort_type, sort_data = sort_type.split('_')
-        courses = eval(f'list(filter(lambda x: x.{sort_type} == "{sort_data}", courses))')
-    else:
-        age = int(sort_type.split('_')[1])
-        courses = list(filter(lambda x: x.age_from <= age < x.age_to, courses))
-    areas = {}
-    directions = set()
     form = RegisterChild()
-    for course in courses:
-        if course.area in areas:
-            if course.direction not in areas[course.area]:
-                areas[course.area].append(course.direction)
-                directions.add(course.direction)
-            else:
-                continue
-        else:
-            areas[course.area] = [course.direction]
-            directions.add(course.direction)
-    directions = list(directions)
+    db_sess = db_session.create_session()
+    courses, areas, directions, nav_areas = show_courses(db_sess)
     if request.method == 'POST':
-        data = request.form.to_dict()
-        pprint(data)
+        pprint(request.form.to_dict())
     return render_template('enroll.html', title='Запись', courses=courses, areas=areas, directions=directions,
-                           form=form, nav_areas=nav_areas)
-
-
-@app.route('/registration', methods=['POST'])
-def registration():
-    if request.method == 'POST':
-        data = request.form.to_dict()
-        pprint(data)
+                           nav_areas=nav_areas, form=form)
 
 
 @app.route('/admin')  # панель администратора
 def admin_panel():
     db_sess = db_session.create_session()
-    courses = db_sess.query(Course).all()
-    return render_template('admin.html', title='Панель администратора', courses=courses)
+    courses, areas, directions, nav_areas = show_courses(db_sess)
+    return render_template('admin.html', title='Панель администратора', courses=courses, areas=areas,
+                           directions=directions, nav_areas=nav_areas)
 
 
 @app.route('/add_course', methods=['GET', 'POST'])  # добавление нового курса
@@ -82,7 +53,28 @@ def add_course():
                             description=form_data['description'][0])
         db_sess.add(new_course)
         db_sess.commit()
+        return redirect('/admin')
     return render_template('add_course.html', title='Добавить новое объединение', form=form)
+
+
+@app.route('/delete_course/<_id>')
+def delete_course(_id):
+    db_sess = db_session.create_session()
+    course = db_sess.query(Course).filter(Course.id == _id).first()
+    db_sess.delete(course)
+    db_sess.commit()
+    return redirect('/admin')
+
+
+@app.route('/redact_course/<_id>', methods=['GET', 'POST'])
+def redact_course(_id):
+    form = NewCourse()
+    db_sess = db_session.create_session()
+    course = db_sess.query(Course).filter(Course.id == _id).first()
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+    return render_template('redact_course.html', course=course, form=form, title='Редактирование объединения')
 
 
 if __name__ == '__main__':
---
+++ showing.py
@@ -0,0 +1,30 @@
+from db.models import Course
+from flask import request
+
+
+def show_courses(db_sess):
+    sort_type = request.args.get('sort_type')
+    courses = db_sess.query(Course).all()
+    nav_areas = list(set([course.area for course in courses]))
+    if not sort_type:
+        pass
+    elif sort_type.split('_')[0] != 'age':
+        sort_type, sort_data = sort_type.split('_')
+        courses = eval(f'list(filter(lambda x: x.{sort_type} == "{sort_data}", courses))')
+    else:
+        age = int(sort_type.split('_')[1])
+        courses = list(filter(lambda x: x.age_from <= age < x.age_to, courses))
+    areas = {}
+    directions = set()
+    for course in courses:
+        if course.area in areas:
+            if course.direction not in areas[course.area]:
+                areas[course.area].append(course.direction)
+                directions.add(course.direction)
+            else:
+                continue
+        else:
+            areas[course.area] = [course.direction]
+            directions.add(course.direction)
+    directions = list(directions)
+    return courses, areas, directions, nav_areas
