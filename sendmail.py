c3a8fbea572e2e16eb3ef048e337bba24dab40fd
Vadim <Oxan4ikDanArt>
2022-05-31 16:33:54 +0500

    Отправка email - в виде html-кода 2

--- sendmail.py
+++ sendmail.py
@@ -1,4 +1,4 @@
-import smtplib
+import smtplib, ssl
 from env import email_login, email_password
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -13,29 +13,27 @@ def send(email: str, subject: str):
     <ul>
         <li>Заявление</li>
         <li>Согласие на фото и видеосъемку</li>
-        <li>Копию свидетельства о рождении ребенка, либо копию паспорта ребенка (для детей старше 14)</li>
+        <li>Копия свидетельства о рождении ребенка, либо копия паспорта ребенка (для детей старше 14)</li>
     </ul>
-    <p>Образцы бланков документов - <a href="priem.ddt-miass.ru/download/documents.zip">скачать</a></p>
+    <a href="http://priem.ddt-miass.ru/download/documents.zip"> Скачать образцы бланков документов </a>
     <p>*Расписание является предварительным, возможна корректировка</p>
     <p>С уважением,</p>
     <p>МАУ ДО "ДДТ "Юность" им.В.П.Макеева"</p>
     <p>наш телефон +73513532265</p>
-    <p>наш сайт  <a href="http://ddt-miass.ru/">http://ddt-miass.ru/</p>
+    <p>наш сайт  <a href="http://ddt-miass.ru/">http://ddt-miass.ru/</a></p>
     <p>ВКонтакте <a href="https://vk.com/ddt_yunost">https://vk.com/ddt_yunost</a></p>
   </body>
 </html>
 """
     address = email_login
     password = email_password
-    server = smtplib.SMTP('smtp.yandex.ru', 587)
-    server.ehlo()   # Кстати, зачем это?
-    server.starttls()
-    server.login(address, password)
     html = MIMEText(text, "html")
     message = MIMEMultipart("alternative")
     message["Subject"] = subject
     message["From"] = address
     message["To"] = email
     message.attach(html)
-    server.sendmail(address, email, html.as_string())
-    server.quit()
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.yandex.ru', 465, context=context) as server:
+        server.login(address, password)
+        server.sendmail(address, email, message.as_string())
