c5358808671b15a464d6d9fb3f5c04502f5108bd
ConstLebedev <kilbdv@yandex.ru>
2022-06-01 23:49:35 +0500

    Парсер 2.0 Удаляет записи, сохраняя данные о counter. Используя нечеткий поиск подставляет counter на место

--- xls_parser.py
+++ xls_parser.py
@@ -3,6 +3,8 @@ import re
 from numbers import Number
 
 from openpyxl import load_workbook
+from thefuzz import process
+
 from db.db_session import global_init, create_session
 from db.models import Course
 from env import path_to_xldata
@@ -10,7 +12,7 @@ from env import path_to_xldata
 FILENAME = path_to_xldata
 
 keys = ['name', 'age_from', 'age_to', 'focus', 'direction', 'description',
-        'teachers', 'area', 'free', 'code', 'schedule']
+        'teachers', 'area', 'free', 'code', 'schedule', 'counter']
 
 
 def clear(s):
@@ -50,11 +52,11 @@ def create_course(name, age, focus, direction, description, teachers, area, free
     if not code:
         code = -1
     if not all((name, age, focus, direction, description, teachers, area, free, code)):
-        return
+        return None
     schedule = dict(filter(lambda pair: pair[1], zip((str(i) for i in range(1, 11)),
                                                      (clear(value) for value in schedule))))
     if not schedule:
-        return
+        return None
     name = name.strip()
     age_from, age_to = get_age(age)
     teachers = teachers.split(',')
@@ -63,19 +65,56 @@ def create_course(name, age, focus, direction, description, teachers, area, free
     area = correct_area(area)
     free = free is None or free.strip().lower() == 'бюджет'
     code = int(code) if code else 0
-    return Course(**dict(zip(keys, (name, age_from, age_to, focus, direction, description, teachers,
-                                    area, free, code, schedule))))
+
+    if not courses:  # таблица курсов была пуста - сразу создаем новый
+        ratio = 0
+    else:
+        find_name, ratio = process.extractOne(name, [c[0] for c in courses])  # нечеткий поиск
+    if ratio > 90:  # на 90% и более похожее название
+        relevant_name = list(filter(lambda t: t[0] == find_name, courses))
+        # есть курсы с одинаковыми названиями, отличаются либо возрастом, либо преподавателями
+        if len(relevant_name) > 1:
+            relevant_age = list(filter(lambda t: (t[1], t[2]) == (age_from, age_to), relevant_name))
+            if len(relevant_age) == 1:
+                key = relevant_age[0]
+            else:
+                relevant_all = list(filter(lambda t: t == (find_name, age_from, age_to, ','.join(teachers)), relevant_name))
+                if relevant_all:
+                    key = relevant_all[0]
+                else:
+                    course = Course(**dict(zip(keys, (name, age_from, age_to, focus, direction, description, teachers,
+                                                      area, free, code, schedule, 0))))
+                    db_session.add(course)
+                    print(f'Ничего не найдено. Создан курс "{name}", counter = 0')
+                    return True
+        else:
+            key = relevant_name[0]
+        counter = courses[key]
+    else:
+        counter = 0
+    course = Course(**dict(zip(keys, (name, age_from, age_to, focus, direction, description, teachers,
+                                      area, free, code, schedule, counter))))
+    db_session.add(course)
+    print(f'Создан курс "{name}", counter = {counter}')
+    return True
 
 
 global_init()
 db_session = create_session()
+courses = {(c.name, c.age_from, c.age_to, ','.join(c.teachers)): c.counter for c in db_session.query(Course).all()}
+print(len(courses))
+db_session.query(Course).delete()
+create_counter = 0
+total_counter = 0
 wb = load_workbook(FILENAME)
 ws = wb.active
 for row in filter(lambda r: r[0].value, ws.iter_rows(min_row=5)):
     if len(list(filter(lambda c: c.value, row))) <= 1:
         continue
-    course = create_course(*(cell.value for cell in row))
-    if not course:
-        continue
-    db_session.add(course)
+    total_counter += 1
+    result = create_course(*(cell.value for cell in row))
+    if result:
+        create_counter += 1
+    print()
 db_session.commit()
+print(f'Создано: {create_counter}, всего прочитано строк: {total_counter}')
