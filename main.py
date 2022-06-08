1c65670f139cd588ab54cd48a00d5cfef9a5d1b3
ConstLebedev <kilbdv@yandex.ru>
2022-06-08 22:54:07 +0500

    роут /registration переделан в соответствии с новой бд. Почему-то не работает autoincrement для Record

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import datetime
 
 from flask import Flask, render_template, request, redirect, url_for, send_file
-from db.models import Course, Registration
+from db.models import Course, Record, Association
 from forms import RegisterChild, AdminEnter
 from showing import show_courses
-from sqlalchemy.orm.attributes import flag_modified
 from sendmail import send
 from env import admin_password
 from sqlalchemy import and_
@@ -41,64 +40,69 @@ def enroll():
 def registration():
     form = RegisterChild()
     args = request.args.to_dict()
-    course_name, group_number = args['course'].replace('23%', '#'), args['group']  # todo: id в запросе (шаблон)
-    course = db_session.query(Course).filter(Course.name == course_name).first()  # todo: фильтр по id
+    course_id, group_number = args['course'], args['group']
+    course = db_session.query(Course).get(course_id)
     if request.method == 'POST':
         data = request.form
-        registered = db_session.query(Registration).filter(
+        child_birthday = datetime.datetime.fromisoformat(data['child_birthday']).date()
+        age = (datetime.date.today() - child_birthday).days // 365
+        if not course.age_from <= age <= course.age_to:
+            return render_template('registration.html', course=course, form=form, group=group_number,
+                                   message='Курс не подходит Вам по возрасту')
+        registered = db_session.query(Record).filter(
             and_(
-                Registration.child_name == data['child_name'].strip().capitalize(),
-                Registration.child_surname == data['child_surname'].strip().capitalize(),
-                Registration.child_patronymic == data['child_patronymic'].strip().capitalize())  # todo: дата рождения
+                Record.child_name == data['child_name'].strip().capitalize(),
+                Record.child_surname == data['child_surname'].strip().capitalize(),
+                Record.child_patronymic == data['child_patronymic'].strip().capitalize(),
+                Record.child_birthday == child_birthday
+            )
         ).first()
         if registered:
-            if any(map(lambda x: data['course_name'] in x, list(registered.courses.keys()))):
+            if any(map(lambda ass: ass.course_id == course_id, registered.courses)):
                 return redirect(url_for('enroll', message_type='danger', message='Вы уже записаны в это объединение!'))
             else:
-                registered.courses[data['course_name']] = data['group']
-                course.counter += 1
-                db_session.add(registered)
-                flag_modified(registered, 'courses')
+                assoc = Association()
+                assoc.group = data['group']
+                assoc.course = course
+                assoc.record = registered
+                db_session.add(assoc)
                 db_session.commit()
                 send(registered.parent_email, 'Запись в ДДТ Юность', course.name, data['group'])
                 return redirect(
                     url_for('enroll', message_type='success', message="Ваша запись успешно зарегистрирована"))
 
         else:
-            # todo: проверка возраста
-            record = Registration(child_name=data['child_name'].strip().capitalize(),
-                                  child_surname=data['child_surname'].strip().capitalize(),
-                                  child_patronymic=data['child_patronymic'].strip().capitalize(),
-                                  child_birthday=datetime.date(int(data['child_birthday'].split('-')[0]),  # todo datetime
-                                                               int(data['child_birthday'].split('-')[1]),
-                                                               int(data['child_birthday'].split('-')[2])),
-                                  educational_institution=data['educational_institution'],
-                                  edu_class=data['edu_class'],
-                                  health=data['health'],
-                                  child_phone=data['child_phone'],
-                                  child_email=data['child_email'],
-                                  child_residence=data['child_residence'],
-                                  parent_name=data['parent_name'],
-                                  parent_surname=data['parent_surname'],
-                                  parent_patronymic=data['parent_patronymic'],
-                                  parent_birthday=datetime.date(int(data['parent_birthday'].split('-')[0]),
-                                                                int(data['parent_birthday'].split('-')[1]),
-                                                                int(data['parent_birthday'].split('-')[2])),
-                                  parent_residence=data['parent_residence'],
-                                  parent_work=data['parent_work'],
-                                  parent_phone=data['parent_phone'],
-                                  parent_email=data['parent_email'],
-                                  full_family=True if data['full_family'] == 'Полная семья' else False,
-                                  large_family=True if data['large_family'] == 'Многодетная семья' else False,
-                                  without_parents=False if data['without_parents'] == 'Нет' else True,
-                                  police_record=False if data['police_record'] == 'Нет' else True,
-                                  resident=False if data['resident'] == 'Нет' else True,
-                                  second_parent_fio=data['second_parent_fio'] if data['second_parent_fio'] else None,
-                                  second_parent_phone=data['second_parent_phone'] if data[
-                                      'second_parent_phone'] else None,
-                                  courses={data['course_name']: data['group']}
-                                  )
-            course.counter += 1  # -
+            record = Record(child_name=data['child_name'].strip().capitalize(),
+                            child_surname=data['child_surname'].strip().capitalize(),
+                            child_patronymic=data['child_patronymic'].strip().capitalize(),
+                            child_birthday=child_birthday,
+                            educational_institution=data['educational_institution'],
+                            edu_class=data['edu_class'],
+                            health=data['health'],
+                            child_phone=data['child_phone'],
+                            child_email=data['child_email'],
+                            child_residence=data['child_residence'],
+                            parent_name=data['parent_name'],
+                            parent_surname=data['parent_surname'],
+                            parent_patronymic=data['parent_patronymic'],
+                            parent_birthday=datetime.datetime.fromisoformat(data['parent_birthday']).date(),
+                            parent_residence=data['parent_residence'],
+                            parent_work=data['parent_work'],
+                            parent_phone=data['parent_phone'],
+                            parent_email=data['parent_email'],
+                            full_family=True if data['full_family'] == 'Полная семья' else False,
+                            large_family=True if data['large_family'] == 'Многодетная семья' else False,
+                            without_parents=False if data['without_parents'] == 'Нет' else True,
+                            police_record=False if data['police_record'] == 'Нет' else True,
+                            resident=False if data['resident'] == 'Нет' else True,
+                            second_parent_fio=data['second_parent_fio'] if data['second_parent_fio'] else None,
+                            second_parent_phone=data['second_parent_phone'] if data[
+                                'second_parent_phone'] else None)
+            assoc = Association()
+            assoc.group = data['group']
+            assoc.course = course
+            assoc.record = record
+            db_session.add(assoc)
             db_session.add(record)
             db_session.commit()
             send(record.parent_email, 'Запись в ДДТ Юность', course.name, data['group'])
