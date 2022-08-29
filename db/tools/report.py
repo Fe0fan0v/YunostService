79c6e3094652b85820b0f7a135d031e7862590c8
ConstLebedev <kilbdv@yandex.ru>
2022-08-29 10:36:12 +0500

    modify report

--- db/tools/report.py
+++ db/tools/report.py
@@ -15,9 +15,15 @@ def get_report(filename):
     ORDER BY id ASC 
     '''
 
+    query_count_by_groups = '''SELECT course_id, "name", area, courses.age_from, courses.age_to, "group", COUNT(record_id) FROM records_courses
+INNER JOIN courses ON course_id = "id"
+GROUP BY course_id, "group", courses."name", area, courses.age_from, courses.age_to
+ORDER BY course_id ASC
+    '''
 
     df_main = pd.read_sql_query(query_all, con=engine)
     df_triples = pd.read_sql_query(query_doubles, con=engine)
+    df_count = pd.read_sql_query(query_count_by_groups, con=engine)
 
     df_main['teachers'] = df_main['teachers'].apply(lambda lst: ', '.join(lst))
     df_main['code'] = df_main['code'].apply(lambda code: {1: 'КУБ', 2: 'УСПЕХ', 3: 3, -1: 'ЮНОСТЬ'}[code])
@@ -25,5 +31,6 @@ def get_report(filename):
     df_main.to_excel(writer, 'общая')
     df_main.loc[df_main.duplicated(subset=['child_name', 'child_surname', 'child_patronymic'], keep=False)].to_excel(writer, 'дубликаты')
     df_triples.to_excel(writer, '3+ направлний')
+    df_count.to_excel(writer, 'кол-во по группам')
 
     writer.save()
