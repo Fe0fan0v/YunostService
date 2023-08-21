459ed1ec138963c7d32630d2ce18b637e0f3ce9a
vadimfeofanov <feofanov@yandexlyceum.ru>
2023-08-21 14:54:13 +0500

    т ть

--- forms.py
+++ forms.py
@@ -16,7 +16,7 @@ class RegisterChild(FlaskForm):
     edu_class = StringField('Класс/курс (на 1 сентября)', validators=[DataRequired()])
     health = SelectField('Здоровье', validators=[DataRequired()], choices=['Здоров', 'ОВЗ', 'Инвалид'])
     snils = StringField('Номер СНИЛС ребенка')
-    certificate = StringField('Номер сертификата вноситься после 31 августа с сайта Госуслуги')
+    certificate = StringField('Номер сертификата вносится после 31 августа с сайта Госуслуги')
     child_phone = StringField('Телефон ребенка')
     child_email = EmailField('Email ребенка')
     child_residence = StringField('Место жительства ребенка', validators=[DataRequired()])
